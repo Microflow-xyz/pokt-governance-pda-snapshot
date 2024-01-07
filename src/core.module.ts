30d3966e46d042d06ad554b8686a9a8752a43400
ahmadyazdanii <ahmad.yazdani.pub@gmail.com>
2024-01-07 14:09:04 +0330

    new [logging system] -> add logging and debugging system

--- src/core.module.ts
+++ src/core.module.ts
@@ -1,13 +1,53 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule, ConfigService } from '@nestjs/config';
+import { APP_FILTER } from '@nestjs/core';
 import { ScheduleModule } from '@nestjs/schedule';
-import { PDAModule } from './pda/pda.module';
+import { format } from 'winston';
+import LokiTransport from 'winston-loki';
 import { ArweaveModule } from '@common/arweave/arweave.module';
+import { AllExceptionsFilter } from '@common/filters/all-exception.filter';
+import { WinstonModule } from '@common/winston/winston.module';
+import { winstonConsoleTransport } from '@common/winston/winston.utils';
+import { PDAModule } from './pda/pda.module';
+import { ScoringModule } from './scoring/scoring.module';
 import { CoreService } from './core.service';
 
 @Module({
   imports: [
     ConfigModule.forRoot({ isGlobal: true }),
+    WinstonModule.forRootAsync({
+      useFactory: (config: ConfigService) => {
+        const grafanaServiceName = config.get<string>('GRAFANA_SERVICE_NAME');
+        const lokiAuth = config.get<string>('GRAFANA_LOKI_AUTH');
+
+        return {
+          level: config.get<string>('DEBUG_MODE') === 'true' ? 'debug' : 'info',
+          format: format.combine(
+            format.ms(),
+            format.timestamp(),
+            format.json(),
+          ),
+          transports:
+            config.get<string>('NODE_ENV') === 'production'
+              ? [
+                  new LokiTransport({
+                    host: config.get<string>('GRAFANA_LOKI_HOST'),
+                    basicAuth: lokiAuth?.length > 0 ? lokiAuth : undefined,
+                    format: format.json(),
+                    labels: {
+                      serviceName:
+                        grafanaServiceName?.length > 0
+                          ? grafanaServiceName
+                          : 'pocket-backend',
+                    },
+                  }),
+                  winstonConsoleTransport,
+                ]
+              : [winstonConsoleTransport],
+        };
+      },
+      inject: [ConfigService],
+    }),
     ScheduleModule.forRoot(),
     ArweaveModule.forRootAsync({
       useFactory: (config: ConfigService) => {
@@ -27,7 +67,11 @@ import { CoreService } from './core.service';
       inject: [ConfigService],
     }),
     PDAModule,
+    ScoringModule,
   ],
-  providers: [CoreService],
+  providers: [
+    { provide: APP_FILTER, useClass: AllExceptionsFilter },
+    CoreService,
+  ],
 })
 export class CoreModule {}
