110d66be0e9a634465700087e159977b71e99699
satiparpar <satiparpar.com@gmail.com>
2024-01-20 14:05:16 +0330

    write unit tests for storeScores method

--- src/common/arweave/__mocks__/arweave.provider.ts
+++ src/common/arweave/__mocks__/arweave.provider.ts
@@ -1,5 +1,6 @@
 export const ArweaveProvider = jest.fn().mockReturnValue({
   getPrice: jest.fn(),
   fund: jest.fn(),
+  storeData: jest.fn(),
   upload: jest.fn(() => Promise.resolve({ id: 'mockedTransactionId' })),
 });
