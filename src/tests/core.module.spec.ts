34fcfbcc5cf6181137bbe84f15e4c6a318532109
satiparpar <satiparpar.com@gmail.com>
2024-01-21 17:03:19 +0330

    write unit test for core.module

---
+++ src/tests/core.module.spec.ts
@@ -0,0 +1,16 @@
+import { Test, TestingModule } from '@nestjs/testing';
+import { CoreModule } from '../core.module';
+
+describe('CoreModule', () => {
+  let module: TestingModule;
+
+  beforeEach(async () => {
+    module = await Test.createTestingModule({
+      imports: [CoreModule],
+    }).compile();
+  });
+
+  it('should be defined', () => {
+    expect(module).toBeDefined();
+  });
+});
