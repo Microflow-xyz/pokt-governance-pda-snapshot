faf3ddadde01523552c12e129cc14abac30760b4
satiparpar <satiparpar.com@gmail.com>
2024-01-21 14:22:51 +0330

    write unit tests for scoring.module

--- src/scoring/__mocks__/scoring.service.ts
+++ src/scoring/__mocks__/scoring.service.ts
@@ -1,3 +1,12 @@
 export const ScoringService = jest.fn().mockReturnValue({
-  calculateScores: jest.fn(),
+  calculateScores: jest.fn(() => {
+    return {
+      gatewayID: {
+        citizen: {
+          point: 0,
+          PDAs: [],
+        },
+      },
+    };
+  }),
 });
