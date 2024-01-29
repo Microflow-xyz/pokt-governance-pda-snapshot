e78c16da95af9bb785391a5e59a7aea484b1f9ef
satiparpar <satiparpar.com@gmail.com>
2024-01-29 18:18:32 +0330

    deleted unnecessary testing files

--- src/common/arweave/__mocks__/arweave.provider.ts
+++ src/common/arweave/__mocks__/arweave.provider.ts
@@ -1,6 +1,6 @@
 export const ArweaveProvider = jest.fn().mockReturnValue({
   getPrice: jest.fn(),
   fund: jest.fn(),
-  storeData: jest.fn(() => Promise.resolve('mockedTransactionId')),
-  upload: jest.fn(() => Promise.resolve({ id: 'mockedTransactionId' })),
+  storeData: jest.fn().mockResolvedValue('mockedTransactionId'),
+  upload: jest.fn().mockResolvedValue({ id: 'mockedTransactionId' }),
 });
