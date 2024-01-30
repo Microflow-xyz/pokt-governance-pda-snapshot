c84a508dba540f72bc9cc5356593fef0acf3c861
satiparpar <satiparpar.com@gmail.com>
2024-01-30 12:44:57 +0330

    update unit tests for arweave.provider

--- src/common/arweave/tests/arweave.provider.spec.ts
+++ src/common/arweave/tests/arweave.provider.spec.ts
@@ -1,5 +1,6 @@
 import Irys from '@irys/sdk';
 import { Test, TestingModule } from '@nestjs/testing';
+import { BigNumber } from 'bignumber.js';
 import { ArweaveTag } from '../interfaces/arweave.interface';
 import { ArweaveProvider } from '../arweave.provider';
 import { IRYS_CONNECTOR } from '../arweave.constant';
@@ -7,18 +8,16 @@ import { IRYS_CONNECTOR } from '../arweave.constant';
 describe('Arweave Provider', () => {
   let provider: ArweaveProvider;
   let irys: Irys;
-
+  const bign = new BigNumber('BigNumber.VALUE');
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
       providers: [
         {
           provide: IRYS_CONNECTOR,
           useValue: {
-            getPrice: jest.fn(),
+            getPrice: jest.fn().mockResolvedValue(bign),
             fund: jest.fn(),
-            upload: jest.fn(() =>
-              Promise.resolve({ id: 'mockedTransactionId' }),
-            ),
+            upload: jest.fn().mockResolvedValue({ id: 'id' }),
           },
         },
         ArweaveProvider,
@@ -27,10 +26,10 @@ describe('Arweave Provider', () => {
 
     provider = module.get<ArweaveProvider>(ArweaveProvider);
     irys = module.get<Irys>(IRYS_CONNECTOR);
+    jest.clearAllMocks();
   });
 
   test('Should be defined', () => {
-    // Assert
     expect(provider).toBeDefined();
   });
 
@@ -47,11 +46,12 @@ describe('Arweave Provider', () => {
       ];
     });
     test('Should be defined', () => {
-      // Assert
       expect(provider['calculateSizeOfData']).toBeDefined();
     });
+    test('Shouldm return 0 when inputs are empty', () => {
+      expect(provider['calculateSizeOfData'](data, tags)).toEqual(0);
+    });
     test('Should loop on tags and calculate them and return number 20', () => {
-      // Arrange
       data = 'data';
       tags = [
         {
@@ -64,38 +64,45 @@ describe('Arweave Provider', () => {
         },
       ];
 
-      // Assert
       expect(provider['calculateSizeOfData'](data, tags)).toEqual(20);
+    });
+    test('Should call byteLength method from Buffer', () => {
+      data = 'data';
+      tags = [
+        {
+          name: 'name',
+          value: 'value',
+        },
+      ];
+      jest.spyOn(Buffer, 'byteLength').mockReturnValue(1);
+      provider['calculateSizeOfData'](data, tags);
+      expect(Buffer.byteLength).toHaveBeenCalledWith('data', 'utf-8');
+      expect(Buffer.byteLength).toHaveBeenCalledWith('name', 'utf-8');
+      expect(Buffer.byteLength).toHaveBeenCalledWith('value', 'utf-8');
+      expect(Buffer.byteLength).toHaveBeenCalledTimes(3);
     });
   });
 
   describe('fundNodeBasedOnSize', () => {
-    let size: number;
     test('Should be defined', () => {
       expect(provider['fundNodeBasedOnSize']).toBeDefined();
     });
-    test('Should call getPrice with correct paremeter', () => {
-      // Arrange
-      size = 10;
-
-      // Act
-      provider['fundNodeBasedOnSize'](size);
-
-      // Assert
-      expect(irys.getPrice).toHaveBeenCalledWith(size);
+    test('Should call getPrice with correct paremeter', async () => {
+      await provider['fundNodeBasedOnSize'](10);
+      expect(irys.getPrice).toHaveBeenCalledWith(10);
+      expect(irys.getPrice).toHaveBeenCalledTimes(1);
+    });
+    test('Should call fund method from irys with correct paremeter', async () => {
+      await provider['fundNodeBasedOnSize'](10);
+      expect(irys.fund).toHaveBeenCalledTimes(1);
+      expect(irys.fund).toHaveBeenCalledWith(bign);
     });
   });
 
   describe('storeData', () => {
     let data: Record<string, any>;
     let tags: Array<ArweaveTag>;
-    let returnValue;
-    test('Should be defined', () => {
-      // Assert
-      expect(provider.storeData).toBeDefined();
-    });
-    test('Should return "mockedTransactionId"', async () => {
-      // Arrange
+    beforeEach(async () => {
       data = { id: 'id' };
       tags = [
         {
@@ -103,12 +110,24 @@ describe('Arweave Provider', () => {
           value: 'vaue',
         },
       ];
+    });
 
-      // Act
-      returnValue = await provider.storeData(data, tags);
-
-      // Assert
-      expect(returnValue).toBe('mockedTransactionId');
+    test('Should be defined', () => {
+      expect(provider.storeData).toBeDefined();
+    });
+    test('Should call stringify method from JSON', async () => {
+      jest.spyOn(JSON, 'stringify').mockReturnValue('');
+      await provider.storeData(data, tags);
+      expect(JSON.stringify).toHaveBeenCalledWith(data);
+    });
+    test('Should fund to Node based on uploading data size', async () => {
+      jest.spyOn(provider as any, 'calculateSizeOfData').mockReturnValue(17);
+      jest.spyOn(provider as any, 'fundNodeBasedOnSize');
+      await provider.storeData(data, tags);
+      expect(provider['fundNodeBasedOnSize']).toHaveBeenCalledWith(17);
+    });
+    test('Should return receipt.id', async () => {
+      expect(await provider.storeData(data, tags)).toEqual('id');
     });
   });
 });
