1bd66700eb902eb7c12541a77a9a73a7b6ceb856
satiparpar <satiparpar.com@gmail.com>
2024-01-31 10:46:57 +0330

    write comment for testing files

--- src/common/filters/tests/all-exception.filter.spec.ts
+++ src/common/filters/tests/all-exception.filter.spec.ts
@@ -3,22 +3,28 @@ import { Test, TestingModule } from '@nestjs/testing';
 import { WinstonProvider } from '@common/winston/winston.provider';
 import { AllExceptionsFilter } from '../all-exception.filter';
 
+// Mock the WinstonProvider
 jest.mock('@common/winston/winston.provider');
 
+// Describe the test suite for the AllExceptionsFilter
 describe('AllExceptionsFilter', () => {
   let filter: AllExceptionsFilter;
   let logger: WinstonProvider;
   let hostMock: ArgumentsHost;
   let applicationRefMock: any;
 
+  // Setup before each test
   beforeEach(async () => {
+    // Create a testing module
     const module: TestingModule = await Test.createTestingModule({
       providers: [AllExceptionsFilter, WinstonProvider],
     }).compile();
 
+    // Initialize instances for testing
     filter = module.get<AllExceptionsFilter>(AllExceptionsFilter);
     logger = module.get<WinstonProvider>(WinstonProvider);
 
+    // Mock the host and applicationRef
     hostMock = {
       getArgByIndex: jest.fn(() => ({ headersSent: false })),
     } as unknown as ArgumentsHost;
@@ -28,20 +34,28 @@ describe('AllExceptionsFilter', () => {
       end: jest.fn(),
     };
 
+    // Clear all mocks before each test
     jest.clearAllMocks();
   });
 
+  // Basic test to check if the filter is defined
   test('Should be defined', () => {
     expect(filter).toBeDefined();
   });
 
+  // Describe the 'handleUnknownError' method tests
   describe('handleUnknownError', () => {
+    // Basic test to check if the method is defined
     test('Should be defined', () => {
       expect(filter.handleUnknownError).toBeDefined();
     });
+
+    // Test to check if the method handles unknown error, logs it, and responds appropriately
     test('Should handle unknown error and log it', () => {
       const exception = new Error('Test error');
       filter.handleUnknownError(exception, hostMock, applicationRefMock);
+
+      // Assertions for response handling and logging
       expect(applicationRefMock.isHeadersSent).toHaveBeenCalled();
       expect(applicationRefMock.reply).toHaveBeenCalledWith(
         expect.anything(),
@@ -57,6 +71,7 @@ describe('AllExceptionsFilter', () => {
       });
     });
 
+    // Test to check if the method ends the response if headers are already sent and calls error method from logger when exception is an object
     test('Should end the response if headers are already sent and call error method from logger when exception is an object', () => {
       const exception = new Error('Test error');
       applicationRefMock.isHeadersSent = jest.fn(() => true);
@@ -66,6 +81,7 @@ describe('AllExceptionsFilter', () => {
       expect(logger.error).toHaveBeenCalledTimes(1);
     });
 
+    // Test to check if the method ends the response if headers are already sent and calls error method from logger when exception is not an object
     test('Should end the response if headers are already sent and call error method from logger when exception is not an object', () => {
       const exception = new Error('Test error');
       applicationRefMock.isHeadersSent = jest.fn(() => true);
--- src/tests/core.service.spec.ts
+++ src/tests/core.service.spec.ts
@@ -5,10 +5,12 @@ import { PDAService } from '../pda/pda.service';
 import { ScoringService } from '../scoring/scoring.service';
 import { StoreService } from '../store/store.service';
 
+// Mock necessary modules and services
 jest.mock('@common/winston/winston.provider');
 jest.mock('../pda/pda.service');
 jest.mock('../store/store.service');
 
+// Describe the test suite for the CoreService
 describe('CoreService', () => {
   let coreService: CoreService;
   let logger: WinstonProvider;
@@ -16,7 +18,9 @@ describe('CoreService', () => {
   let pdaService: PDAService;
   let storeService: StoreService;
 
+  // Setup before each test
   beforeEach(async () => {
+    // Create a testing module with the required providers
     const module: TestingModule = await Test.createTestingModule({
       providers: [
         CoreService,
@@ -27,26 +31,36 @@ describe('CoreService', () => {
       ],
     }).compile();
 
+    // Initialize instances for testing
     coreService = module.get<CoreService>(CoreService);
     logger = module.get<WinstonProvider>(WinstonProvider);
     scoringService = module.get<ScoringService>(ScoringService);
     pdaService = module.get<PDAService>(PDAService);
     storeService = module.get<StoreService>(StoreService);
 
+    // Clear all mocks before each test
     jest.clearAllMocks();
   });
+
+  // Basic test to check if the service is defined
   test('Should be defined', () => {
     expect(coreService).toBeDefined();
   });
 
+  // Describe the 'handler' method tests
   describe('handler', () => {
+    // Setup before each test
     beforeEach(async () => {
       await coreService.handler();
     });
+
+    // Basic test to check if the method is defined
     test('Should be defined', () => {
       expect(coreService).toBeDefined();
     });
-    test('Should Get all issued PDAs and call debug method from logger using all issued PDA ', async () => {
+
+    // Test to check if the method calls getIssuedPDAs method from PDAService and debug method from logger
+    test('Should Get all issued PDAs and call debug method from logger using all issued PDA', async () => {
       await coreService.handler();
       expect(pdaService.getIssuedPDAs).toHaveBeenCalled();
       expect(logger.debug).toHaveBeenCalledWith(
@@ -54,6 +68,8 @@ describe('CoreService', () => {
         CoreService.name,
       );
     });
+
+    // Test to check if the method calls calculateScores method from ScoringService and debug method from logger
     test('Should calculate scores and call debug method from logger using scores', async () => {
       jest.spyOn(scoringService, 'calculateScores').mockReturnValue({});
       await coreService.handler();
@@ -63,6 +79,8 @@ describe('CoreService', () => {
         CoreService.name,
       );
     });
+
+    // Test to check if the method calls storeScores method from StoreService and log method from logger
     test('Should Store Scores and PDAs and and call log method from logger using arweaveURL', async () => {
       await coreService.handler();
       expect(storeService.storeScores).toHaveBeenCalled();
