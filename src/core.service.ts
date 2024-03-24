470054610f5e6552a8f277df3b021b460c873b73
ahmadyazdanii <ahmad.yazdani.pub@gmail.com>
2024-03-24 14:12:08 +0330

    update [core]: change error handler mechanism

--- src/core.service.ts
+++ src/core.service.ts
@@ -14,27 +14,32 @@ export class CoreService {
   ) {}
 
   async handler() {
-    this.logger.log('The job executed', CoreService.name);
-    // Get all issued PDAs
-    const allPDAs = await this.pdaService.getIssuedPDAs();
+    try {
+      this.logger.log('The job executed', CoreService.name);
+      // Get all issued PDAs
+      const allPDAs = await this.pdaService.getIssuedPDAs();
 
-    this.logger.debug(
-      'All issued PDAs:\n' + JSON.stringify(allPDAs),
-      CoreService.name,
-    );
-    // calculate scores
-    const scores = this.scoringService.calculateScores(allPDAs);
+      this.logger.debug(
+        'All issued PDAs:\n' + JSON.stringify(allPDAs),
+        CoreService.name,
+      );
+      // calculate scores
+      const scores = this.scoringService.calculateScores(allPDAs);
 
-    this.logger.debug(
-      "All issued PDAs' scores:\n" + JSON.stringify(scores),
-      CoreService.name,
-    );
-    // Store Scores & PDAs
-    const arweaveURL = await this.storeService.storeScores(scores);
+      this.logger.debug(
+        "All issued PDAs' scores:\n" + JSON.stringify(scores),
+        CoreService.name,
+      );
+      // Store Scores & PDAs
+      const arweaveURL = await this.storeService.storeScores(scores);
 
-    this.logger.log(
-      `The job finished. storage URL is (${arweaveURL})`,
-      CoreService.name,
-    );
+      this.logger.log(
+        `The job finished. storage URL is (${arweaveURL})`,
+        CoreService.name,
+      );
+    } catch (err) {
+      this.logger.error(err.message, CoreService.name, { stack: err.stack });
+      throw err;
+    }
   }
 }
